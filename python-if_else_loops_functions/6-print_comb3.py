9f4b9deb88f97667208ac907289495cdba513782
ThamerAyachi <thamerjak3@gmail.com>
2023-01-17 13:34:36 +0100

    fix output 00

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(9):
     for j in range(i, 10):
         if i == 8 and j == 9:
             print("{}".format(i*10+j))
-        elif (i != 0) and i == j:
+        elif i == j:
             continue
         else:
             print("{:02d}".format(i*10+j), end=", ")
