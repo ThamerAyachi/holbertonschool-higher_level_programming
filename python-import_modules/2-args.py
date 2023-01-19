7308c334174df0ec7e8b6998d3ae20497acbca54
ThamerAyachi <thamerjak3@gmail.com>
2023-01-19 19:50:58 +0100

    fix number bug

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
+    if(len(sys.argv) - 1 == 1):
+        print("1 argument:")
+    elif len(sys.argv) == 0:
+        print("0 argument.")
+        
     print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1 , len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
