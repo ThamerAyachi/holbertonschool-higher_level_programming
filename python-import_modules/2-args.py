ed137faf45f166b7abc3305d672b4b804b8d17b4
ThamerAyachi <thamerjak3@gmail.com>
2023-01-19 19:48:26 +0100

    fix word again

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
 
-    print("{} argument:".format(len(sys.argv) - 1))
+    print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1 , len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
