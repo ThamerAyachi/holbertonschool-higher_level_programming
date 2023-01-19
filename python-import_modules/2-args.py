949512386848e3520f02ee2aa00575f88ac304e7
ThamerAyachi <thamerjak3@gmail.com>
2023-01-19 19:53:30 +0100

    fix style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if(len(sys.argv) - 1 == 1):
+    if len(sys.argv) - 1 == 1:
         print("1 argument:")
-    elif len(sys.argv) == 0:
+    elif len(sys.argv) == 1:
         print("0 argument.")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1 , len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
