03bbd0d04d4aa486bfd2001ad40154b477630f07
ThamerAyachi <thamerjak3@gmail.com>
2023-01-19 19:54:10 +0100

    add fucking 0

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 == 1:
         print("1 argument:")
     elif len(sys.argv) == 1:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
