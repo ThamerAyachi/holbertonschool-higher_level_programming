bf411dfd71d90b6a5859bef5fbd44d62b268e224
ThamerAyachi <thamerjak3@gmail.com>
2023-01-19 20:47:19 +0100

    another method

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-from importlib import import_module
-
-def main():
-    module = import_module("variable_load_5")
-    a = module.a
-    print(a)
-
 if __name__ == "__main__":
-    main()
+    import variable_load_5
+    a = variable_load_5.a
+    print(a)
