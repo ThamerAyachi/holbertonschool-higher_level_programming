2233b0a90f94bbc74efe1ac35673f9fb583cd644
ThamerAyachi <thamerjak3@gmail.com>
2023-01-30 19:41:18 +0100

    add document

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Fist Document"""
+
 class Square:
     """Empty class that defines a square"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Fist Document"""
+
 class Square:
     """Defines a square"""
     def __init__(self, size):
