12122a213c32101fc2fce0d41df45c280713f8be
ThamerAyachi <thamerjak3@gmail.com>
2023-02-01 21:56:54 +0100

    add task 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module have one function"""
+
+
+def matrix_divided(matrix, div):
+    """matrix divided"""
+    if not all(isinstance(i, (int, float)) for row in matrix for i in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in row] for row in matrix]
