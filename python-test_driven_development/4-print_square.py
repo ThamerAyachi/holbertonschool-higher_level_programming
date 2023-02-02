2a38bf7413a81730680ff4e7d4985b0361b73093
ThamerAyachi <thamerjak3@gmail.com>
2023-02-02 10:29:46 +0100

    add task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module have print_square"""
+
+
+def print_square(size) -> None:
+    """_summary_
+
+    Args:
+        size (integer)
+
+    Raises:
+        TypeError: size must be an integer
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    """
+    if not (isinstance(size, int) or isinstance(size, float)):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
+    else:
+        for i in range(size):
+            print("#" * size)
