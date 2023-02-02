a5106c0f05b806edff9ba4e9653a518fada87e48
ThamerAyachi <thamerjak3@gmail.com>
2023-02-02 10:30:58 +0100

    fix bug

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -19,8 +19,5 @@ def print_square(size) -> None:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
-    else:
-        for i in range(size):
-            print("#" * size)
+    for i in range(size):
+        print("#" * size)
