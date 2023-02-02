b257286f55709a98966fb616f9de3d0bc7ec17e7
ThamerAyachi <thamerjak3@gmail.com>
2023-02-02 10:53:36 +0100

    add task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module have text_indentation"""
+
+
+def text_indentation(text) -> None:
+    """_summary_
+
+    Args:
+        text (string)
+
+    Raises:
+        TypeError: text must be a string
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    for char in text:
+        if char in ".?:":
+            new_text += char + "\n\n"
+        else:
+            new_text += char
+
+    lines = new_text.split("\n")
+    for line in lines:
+        print(line.strip())
