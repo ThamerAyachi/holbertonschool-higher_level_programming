bd8aa1bbd1aa4443c135636ce52e380f6179c459
ThamerAyachi <thamerjak3@gmail.com>
2023-02-05 00:23:50 +0100

    fix task 100

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -12,7 +12,7 @@ class Node:
             raise TypeError("data must be an integer")
         self.__data = data
 
-        if not isinstance(next_node, Node):
+        if not isinstance(next_node, Node) and next_node is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = next_node
 
@@ -38,9 +38,27 @@ class Node:
 
 
 class SinglyLinkedList:
-    """Singly Linked List class"""
+    def __init__(self):
+        self.head = None
 
-    __head: Node
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        elif self.head.data >= value:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current: Node = self.head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
 
-    def __init__(self) -> None:
-        pass
+    def __str__(self):
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next_node
+        return "\n".join(result)
