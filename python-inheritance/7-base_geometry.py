47dbdf012cfc87e6a7216222ce55c0174a7af480
ThamerAyachi <thamerjak3@gmail.com>
2023-02-09 20:31:22 +0100

    add task 7 without test

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""BaseGeometry Module"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+
+    def area(self) -> None:
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name: str, value) -> None:
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
