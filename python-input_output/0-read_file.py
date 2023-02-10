ad7ae8827f2991f21cb1acdf8a03d8fd014fcb95
ThamerAyachi <thamerjak3@gmail.com>
2023-02-10 16:15:18 +0100

    add task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Read file"""
+
+
+def read_file(filename=""):
+    """Read file function"""
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            print(line, end="")
+    print()
