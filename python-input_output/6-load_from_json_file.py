ecff3196aebc265e6a14059bf448383c3ddd55ec
ThamerAyachi <thamerjak3@gmail.com>
2023-02-10 19:30:28 +0100

    try fix bbug

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Get Obj from json file function"""
-    with open(filename, "r", encoding='utf8') as json_file:
-        obj = json_file.readlines()
-        return json.loads(obj)
+    with open(filename) as json_file:
+        return json.loads(json_file)
