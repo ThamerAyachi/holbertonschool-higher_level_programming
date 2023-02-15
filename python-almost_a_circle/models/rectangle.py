73e024513ca2ba0364bb470d90c69d52cee6785b
ThamerAyachi <thamerjak3@gmail.com>
2023-02-15 13:16:52 +0100

    add task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,8 +95,9 @@ class Rectangle (Base):
 
     def display(self):
         """ prints in stdout the Rectangle instance """
+        print("\n"*self.__y, end="")
         for i in range(self.__height):
-            print("#"*self.__width)
+            print(" "*self.__x + "#"*self.__width)
 
     def __str__(self) -> str:
         """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
