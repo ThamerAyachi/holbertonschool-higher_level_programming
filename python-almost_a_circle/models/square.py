9a7dca01cb6e3abaebc30327979d5e41454d9abe
ThamerAyachi <thamerjak3@gmail.com>
2023-02-15 14:04:36 +0100

    add task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,3 +14,23 @@ class Square (Rectangle):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.height
         )
+
+    @property
+    def size(self) -> int:
+        """get size"""
+        return self.width
+
+    @size.setter
+    def size(self, value) -> None:
+        """Set size"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if not value > 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if not value > 0:
+            raise ValueError("height must be > 0")
+        self.height = value
