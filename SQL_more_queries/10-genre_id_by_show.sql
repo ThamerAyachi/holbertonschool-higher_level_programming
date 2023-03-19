29a5bca6d3c1479f7eadfc9fcbe71d7a9f7c970d
ThamerAyachi <thamerjak3@gmail.com>
2023-03-19 20:05:34 +0100

    task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Select Tables
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
