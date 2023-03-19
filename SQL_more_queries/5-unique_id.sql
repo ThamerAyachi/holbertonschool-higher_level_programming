33601a43668e60f1dbef3152d01ff27970d0d26e
ThamerAyachi <thamerjak3@gmail.com>
2023-03-19 19:22:01 +0100

    task 6

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -3,5 +3,6 @@
 CREATE TABLE IF NOT EXISTS unique_id (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+    UNIQUE KEY unique_id_idx (id)
 );
