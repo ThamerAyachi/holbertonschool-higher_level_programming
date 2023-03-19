d9c0ad6f796c9f4fa7a835ea29dc96634f3b269b
ThamerAyachi <thamerjak3@gmail.com>
2023-03-19 19:43:25 +0100

    task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- Select Item by item from another table
+
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = "California" ORDER BY cities.id ASC;
