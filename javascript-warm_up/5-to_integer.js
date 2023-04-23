20c9570a149e1dfee089e842ec197da72965a883
ThamerAyachi <thamerjak3@gmail.com>
2023-04-23 18:20:46 +0100

    add ":" in task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const args = process.argv.slice(2);
 
 const passedInt = parseInt(args[0]);
 
-if (!isNaN(passedInt)) console.log('My number', passedInt);
+if (!isNaN(passedInt)) console.log('My number:', passedInt);
 else console.log('Not a number');
