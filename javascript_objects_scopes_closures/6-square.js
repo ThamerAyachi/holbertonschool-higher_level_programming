0218dc4d38145e90cb58653e0138f0045c1d4dab
ThamerAyachi <thamerjak3@gmail.com>
2023-04-24 23:03:53 +0100

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+	constructor(size) {
+		super(size, size);
+	}
+
+	double() {
+		super.double();
+	}
+
+	charPrint(c = 'X') {
+		super.print(c);
+	}
+};
