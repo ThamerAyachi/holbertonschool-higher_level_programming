176fad6b12975b0c028fcd4e371593b357cfe9a1
ThamerAyachi <thamerjak3@gmail.com>
2023-04-24 22:39:25 +0100

    task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(
+	process.argv[2]
+);
+
+request(starWarsUri, (_err, _res, body) => {
+	body = JSON.parse(body);
+	console.log(body.title);
+});
