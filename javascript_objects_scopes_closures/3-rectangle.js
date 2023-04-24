506f44940fce0627dd3545ed7f0945baab572632
ThamerAyachi <thamerjak3@gmail.com>
2023-04-24 22:59:44 +0100

    task 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+	constructor(width, height) {
+		if (
+			typeof width === 'number' &&
+			typeof height === 'number' &&
+			width > 0 &&
+			height > 0
+		)
+			Object.assign(this, { width, height });
+	}
+
+	print() {
+		console.log('X'.repeat(this.width).concat('\n').repeat(this.height));
+	}
+};
